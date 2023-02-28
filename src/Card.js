615db626aec485c0969bf88aae05a66c9a10a816
beka shamugia <68068249+beqa1414@users.noreply.github.com>
2023-02-28 20:34:09 +0400

    add styles module

--- src/Card.js
+++ src/Card.js
@@ -4,37 +4,37 @@ import Comb_shape from "./images/Combined Shape.png";
 import shape from "./images/Shape.png";
 import oval_img from "./images/Oval.png";
 import shape_1 from "./images/Shape (1).png";
-import "./Card.css";
+import styles from "./Card.module.css";
 
 function Card() {
   return (
-    <div className="card">
-      <div className="image">
-        <img src={Rect} alt="" className="main-img" />
-        <div className="eye">
-          <img src={shape_1} alt="" className="eye-img" />
+    <div className={styles.card}>
+      <div className={styles.image}>
+        <img src={Rect} alt="" className={styles.main_img} />
+        <div className={styles.eye}>
+          <img src={shape_1} alt="" className={styles.eye_img} />
         </div>
       </div>
       <div className="equil">
         <h3>Equilibrium #3429</h3>
       </div>
-      <div className="our-eqiul">
+      <div className={styles.our_eqiul}>
         Our Equilibrium collection promotes <span>balance and calm.</span>
       </div>
-      <div className="days-price">
-        <div className="price">
+      <div className={styles.days_price}>
+        <div className={styles.price}>
           <img
             src={Comb_shape}
-            alt="comb-shap"
+            alt=""
             style={{ width: "11px", height: "18px" }}
           />
           0.041 ETH
         </div>
-        <div className="days">
+        <div className={styles.days}>
           <img src={shape} alt="" />3 days left
         </div>
       </div>
-      <div className="oval-img">
+      <div className={styles.oval_img}>
         <img src={oval_img} alt="" className="avatar" />
         Creation of <span>Jules Wyvern</span>
       </div>
